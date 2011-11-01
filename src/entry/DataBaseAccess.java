bb632bc06e6682bd52930942a2e02ba5e264866d
ryosuke shimomura <r-shimomura@esm.co.jp>
2011-11-02 01:49:15 +0900

    データベースとその登録機能の作成

--- src/entry/DataBaseAccess.java
+++ src/entry/DataBaseAccess.java
@@ -1,8 +1,19 @@
 package entry;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class DataBaseAccess {
+
+    private static Set<String> set = new HashSet<String>();
 
     public static boolean SearchResult(String input) {
         return input.equals("java.lang");
+    }
+
+    public static Set<String> Add(String entry) {
+//        Set<String> set =new HashSet<String>();
+        set.add(entry);
+        return set;
     }
 }
--- test/entry/DataBaseAccessTest.java
+++ test/entry/DataBaseAccessTest.java
@@ -2,6 +2,8 @@ package entry;
 
 import static org.junit.Assert.*;
 
+import java.util.Set;
+
 import org.junit.Test;
 
 public class DataBaseAccessTest {
@@ -10,6 +12,20 @@ public class DataBaseAccessTest {
     public void SearchResultTest() {
         String input = "java.lang";
         assertTrue(DataBaseAccess.SearchResult(input));
+    }
+
+    @Test
+    public void AddTest() {
+        String entry="entry";
+        Set<String> set = DataBaseAccess.Add(entry);
+        assertEquals(1 ,set.size());
+        assertTrue(DataBaseAccess.Add(entry).contains(entry));
+
+        String entry2="entry2";
+        set = DataBaseAccess.Add(entry2);
+        assertEquals(2, set.size());
+        assertTrue(DataBaseAccess.Add(entry2).contains(entry2));
+        assertTrue(DataBaseAccess.Add(entry).contains(entry));
     }
 
 }
