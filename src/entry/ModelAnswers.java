633c3e70207deb7d91a81500251ccd71bfbaa6e3
ryosuke shimomura <r-shimomura@esm.co.jp>
2011-11-23 22:29:24 +0900

    外部から答えを追加する機能の削除

--- src/entry/ModelAnswers.java
+++ src/entry/ModelAnswers.java
@@ -1,17 +1,14 @@
 package entry;
 
-import java.util.HashSet;
 import java.util.Set;
+
+import theme.JavaLang;
 
 public class ModelAnswers {
 
-    private Set<String> modelAnswers;
-    public ModelAnswers(){
-        modelAnswers = new HashSet<String>();
-    }
+    private Set<String> modelAnswers = JavaLang.getInstance().javaLang;
 
-    public ModelAnswers(Set<String> themeAnswers) {
-        modelAnswers = themeAnswers;
+    public ModelAnswers(){
     }
 
     public boolean isTheSame(String answer) {
@@ -22,11 +19,7 @@ public class ModelAnswers {
         return answer;
     }
 
-    public void addModelAnswers(Set<String> newModelAnswers) {
-        modelAnswers = newModelAnswers;
-    }
-
-    public Set<String> addModelAnswer(String newModelAnswer) {
+    private Set<String> addModelAnswer(String newModelAnswer) {
         modelAnswers.add(newModelAnswer);
         return modelAnswers;
     }
--- test/entry/ModelAnswersTest.java
+++ test/entry/ModelAnswersTest.java
@@ -2,74 +2,22 @@ package entry;
 
 import static org.junit.Assert.*;
 
-import java.util.HashSet;
-import java.util.Set;
-
 import org.junit.Test;
-import theme.JavaLang;
 
 public class ModelAnswersTest {
 
     @Test
     public void testJudgeForOnce() {
-        ModelAnswers modelAnswers = new ModelAnswers(JavaLang.getInstance().javaLang);
+        ModelAnswers modelAnswers = new ModelAnswers();
         assertTrue(modelAnswers.isTheSame("Integer"));
         assertFalse(modelAnswers.isTheSame("Integer"));
     }
 
     @Test
-    public void testThemeSelect() {
-        ModelAnswers modelAnswers = new ModelAnswers(JavaLang.getInstance().javaLang);
+    public void testCheckInputAnswer() {
+        ModelAnswers modelAnswers = new ModelAnswers();
         assertTrue(modelAnswers.isTheSame("Integer"));
         assertFalse(modelAnswers.isTheSame("List"));
     }
 
-    @Test
-    public void resultOfSolutionTest() {
-        ModelAnswers modelAnswers = new ModelAnswers();
-        modelAnswers.addModelAnswer("java.lang");
-        assertTrue(modelAnswers.isTheSame("java.lang"));
-        assertFalse(modelAnswers.isTheSame("incorrect_answer"));
-    }
-
-    @Test
-    public void addNewModelAnswerTest() {
-        ModelAnswers modelAnswers = new ModelAnswers();
-
-        Set<String> newModelAnswers = modelAnswers.addModelAnswer("newModelAnswer");
-        assertEquals(1 ,newModelAnswers.size());
-        assertTrue(newModelAnswers.contains("newModelAnswer"));
-
-        newModelAnswers = modelAnswers.addModelAnswer("util");
-        assertEquals(2, newModelAnswers.size());
-        assertTrue(newModelAnswers.contains("util"));
-        assertTrue(newModelAnswers.contains("newModelAnswer"));
-    }
-
-    @Test
-    public void addNewModelMultipleCaseTest(){
-        ModelAnswers modelAnswers = new ModelAnswers();
-        String[] addModelAnswer = {"entry1", "entry2"};
-
-        Set<String> addMultipleAnswer;
-        for(String answer:addModelAnswer)
-            addMultipleAnswer = modelAnswers.addModelAnswer(answer);
-        addMultipleAnswer = modelAnswers.addModelAnswer(addModelAnswer[0]);
-
-        assertEquals(2,addMultipleAnswer.size());
-    }
-
-    @Test
-    public void addNewModelAnswersTest() {
-        ModelAnswers modelAnswers = new ModelAnswers();
-        Set<String> newModels = new HashSet<String>();
-        newModels.add("Integer");
-        newModels.add("String");
-        newModels.add("Boolean");
-        modelAnswers.addModelAnswers(newModels);
-
-        assertTrue(modelAnswers.isTheSame("String"));
-        assertTrue(modelAnswers.isTheSame("Integer"));
-        assertFalse(modelAnswers.isTheSame("List"));
-    }
 }
